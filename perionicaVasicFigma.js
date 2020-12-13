8e83e53a2fdedbe3f1b941d7ca0100e2e368fc9a
MarkoVasicLesnica <perionicavasic2018@gmail.com>
2020-12-13 23:51:21 +0100

    13.12
    13.12

--- perionicaVasicFigma.js
+++ perionicaVasicFigma.js
@@ -1,14 +1,27 @@
 
 const slike = document.querySelectorAll('#div_kamioni img');
-const width = slike[0].clientWidth
 
-var counter = 0;
+var counter = 1;
+var modal = 10;
 
-setInterval(() => {
+setInterval( () => {
+    var margina = -100 * counter
+
+    if(counter < 10){
+        slike[0].style.marginLeft = margina + '%';
+        counter++;
+        modal--;
+    }else{
+        if(modal < 10){
+            modal++;
+            slike[0].style.marginLeft = margina + 100 * modal + '%';
+        }else{
+            counter = 1;
+        }
+    }
     
-    slike[counter].style.marginRight = '-567px';
-    counter++;
-    console.log(counter);
-    
-    return counter;
 },3000)
+    
+
+
+//probati sa klasama
